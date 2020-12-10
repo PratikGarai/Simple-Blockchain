12385e1c66f0579e83815cc50cd4d015d6337af2
PratikGarai <pratikgarai0208@gmail.com>
2020-12-11 04:07:20 +0530

    added some stuff of v3

--- Transaction.js
+++ Transaction.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Transaction
 {
     constructor(fromAddress, toAddress, amount)
@@ -5,6 +7,28 @@ class Transaction
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
+    }
+
+    calculateHash()
+    {
+        return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+    }
+
+    signTransaction(signingKey)
+    {
+        if(signingKey.getPublic('hex') !== this.fromAddress)
+            throw new Error('You cannot use this wallet');
+
+        const hashtx = this.calculateHash();
+        const sig = signingKey.sign(hashtx, 'base64');
+        this.signature = sig.toDER('hex');
+    }
+
+    isValid()
+    {
+        if(this.fromAddress===null) 
+            return true;
+        
     }
 }
 
