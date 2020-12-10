f728b557f11b829899e1b1255a497bbc6073e199
PratikGarai <pratikgarai0208@gmail.com>
2020-12-10 18:23:03 +0530

    added blockchain class

--- Main.js
+++ Main.js
@@ -16,3 +16,28 @@ class Block
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
+
+class BlockChain
+{
+    constructor()
+    {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock()
+    {
+        return new Block(0, "01/01/2021", "Genesis block", "0");
+    }
+
+    getLatestBlock()
+    {
+        return this.chain[this.chain.length-1];
+    }
+
+    addBlock(newBlock)
+    {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
