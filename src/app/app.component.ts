027b970c2e99c1fe5bf55d7d3fcc3f1a31a61e0c
PratikGarai <pratikgarai0208@gmail.com>
2021-02-01 11:59:28 +0530

    service written

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simpleblockchain';
+
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/services/blockchain.service.ts
+++ src/app/services/blockchain.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import BlockChain from './blockchain/BlockChain';
+import EC from 'elliptic';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlockchainService {
 
-  constructor() { }
+  public blockchainInstance = new BlockChain();
+  public walletKeys = [];
+
+  constructor() { 
+    this.blockchainInstance.difficulty = 1;
+    this.blockchainInstance.minePendingTransactions('my-wallet-address');
+
+    this.generateWalletKeys();
+  }
+
+  private generateWalletKeys() : void {
+    const ec = new EC('secp256k1');
+    const key = ec.genKeyPair();
+
+    this.walletKeys.push({
+      keyObj : key,
+      publicKey : key.getPublic('hex'),
+      privateKey : key.getPrivate('hex'),
+    });
+  }
+
+  private getBlocks() {
+    return this.blockchainInstance.chain;
+  }
 }
